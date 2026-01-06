1505ade16f635f8e8231c755d120bb3a33359fb6
Gabriele Toselli <toselli.gabriele@gmail.com>
2026-01-06 19:22:02 +0100

    booking decider functions

--- src/booking/decide.ts
+++ src/booking/decide.ts
@@ -2,36 +2,44 @@ import type { Command, Event, State } from './types';
 
 export function decide(cmd: Command, state: State): Event[] {
   switch (cmd.type) {
-    case 'SCHEDULE_EVENT': {
-      if (state.status === 'scheduled') throw new Error('Event already scheduled');
-      return [
-        {
-          data: { id: state.id, startAt: cmd.data.startAt, patientId: cmd.data.patientId, scheduledAt: new Date() },
-          type: 'EVENT_SCHEDULED',
-        },
-      ];
-    }
-    case 'CANCEL_EVENT': {
-      if (state.status === 'initial') throw new Error('Event not scheduled');
-      if (state.cancelledAt) throw new Error('Event already cancelled');
-
-      return [
-        {
-          data: { cancelledAt: new Date() },
-          type: 'EVENT_CANCELLED',
-        },
-      ];
-    }
-    case 'RESCHEDULE_EVENT': {
-      if (state.status === 'initial') throw new Error('Event not scheduled');
-      if (state.cancelledAt) throw new Error('Event cancelled');
-
-      return [{ data: { startAt: cmd.data.startAt }, type: 'EVENT_RESCHEDULED' }];
-    }
-
+    case 'SCHEDULE_EVENT':
+      return scheduleEvent(state, cmd.data.startAt, cmd.data.patientId);
+    case 'CANCEL_EVENT':
+      return cancelEvent(state);
+    case 'RESCHEDULE_EVENT':
+      return rescheduleEvent(state, cmd.data.startAt);
     default: {
       const _exhaustive: never = cmd;
       return _exhaustive;
     }
   }
+}
+
+function scheduleEvent(state: State, startAt: Date, patientId: string): Event[] {
+  if (state.status === 'scheduled') throw new Error('Event already scheduled');
+  return [
+    {
+      data: { id: state.id, startAt, patientId, scheduledAt: new Date() },
+      type: 'EVENT_SCHEDULED',
+    },
+  ];
+}
+
+function cancelEvent(state: State): Event[] {
+  if (state.status === 'initial') throw new Error('Event not scheduled');
+  if (state.cancelledAt) throw new Error('Event already cancelled');
+
+  return [
+    {
+      data: { cancelledAt: new Date() },
+      type: 'EVENT_CANCELLED',
+    },
+  ];
+}
+
+function rescheduleEvent(state: State, startAt: Date): Event[] {
+  if (state.status === 'initial') throw new Error('Event not scheduled');
+  if (state.cancelledAt) throw new Error('Event cancelled');
+
+  return [{ data: { startAt }, type: 'EVENT_RESCHEDULED' }];
 }
